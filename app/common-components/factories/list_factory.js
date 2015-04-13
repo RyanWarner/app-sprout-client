ccf550899209217b5acc47b71fe1feafe999f6ce
Ryan Warner <warner.ryan@gmail.com>
2015-04-13 14:14:35 -0700

    get list api call

--- app/common-components/factories/list_factory.js
+++ app/common-components/factories/list_factory.js
@@ -35,6 +35,32 @@ listFactory.factory( 'listFactory', function( $http, $q, $state, appConstants )
 		return promise;
 	};
 
+	listFactoryApi.getList = function(  )
+	{
+		var deferred = $q.defer(  );
+		var promise = deferred.promise;
+
+		$http( {
+
+			method: 'get',
+			url: appConstants.BACKEND_URL + '/api/user/list'
+
+		} )
+		.success( function( data )
+		{
+			console.log( 'Get list success: ', data );
+
+			deferred.resolve( data.body.listItems );
+		} )
+		.error( function( error )
+		{
+			console.log( 'Get list error: ', error );
+			deferred.reject( error );
+		} );
+
+		return promise;
+	};
+
 
 	// Return public API.
 
--- app/states/app-root/list/list_controller.js
+++ app/states/app-root/list/list_controller.js
@@ -2,13 +2,25 @@
 
 var list = angular.module( 'list' );
 
-list.controller( 'ListController', function( $rootScope, $scope )
+list.controller( 'ListController', function( $rootScope, $scope, listFactory )
 {
-
 	// This is a controller.
 
 	$scope.stateName = 'list';
 
 	console.log( 'ListController active!' );
+
+	$scope.list = [  ];
+
+	var getList = function(  )
+	{
+		listFactory.getList(  )
+		.then( function( list )
+		{
+			$scope.list = list;
+		} );
+	};
+
+	getList(  );
 
 } );
