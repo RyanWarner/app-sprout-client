e560f85c9b456725fb3b866833a0d7d9fb18fc7e
Ryan Warner <warner.ryan@gmail.com>
2015-04-23 17:05:09 -0700

    fix missing selenium jar by installing selenium standalone jar

--- tests/protractor.config.js
+++ tests/protractor.config.js
@@ -1,7 +1,8 @@
 exports.config =
 {
-    seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.44.0.jar',
-	capabilities: {
+    seleniumServerJar: '../node_modules/selenium-standalone-jar/bin/selenium-server-standalone-2.45.0.jar',
+	capabilities:
+	{
 		'browserName': 'chrome'
 	}
 };
