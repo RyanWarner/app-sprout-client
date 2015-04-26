8aa504c0b9d54f66d7030ddae58bdcb6a025d5d7
Ryan Warner <warner.ryan@gmail.com>
2015-04-26 09:14:14 -0700

    add test for updating a list item

--- app/states/app-root/list/list_test-e2e.js
+++ app/states/app-root/list/list_test-e2e.js
@@ -20,34 +20,46 @@ describe( 'App List', function(  )
 
 			$httpBackend.whenPOST( '/api/user/list' ).respond( function( method, url, data, headers )
 			{
-				console.log( 'POST' );
-
-				console.log( 'POST' );
-
 				return [ 200, response, {  } ];
 			} );
-  		} );
+		} );
 
-  		browser.addMockModule( 'httpBackendMock', httpBackendMock );
+		browser.addMockModule( 'httpBackendMock', httpBackendMock );
 	};
 
-		
 
-
-	it( 'should add a todo', function(  )
+	it( 'should add an item to the list', function(  )
 	{
-		browser.ignoreSynchronization = false;
 		browser.get( 'http://localhost:8080/app/list' );
-		browser.waitForAngular(  );
-		browser.manage(  ).window(  ).maximize(  );
+		// browser.waitForAngular(  );
+		// browser.manage(  ).window(  ).maximize(  );
 
 		element( by.model( 'newItem' ) ).sendKeys( 'new list item' );
 		element( by.css( 'div.enter-icon' ) ).click(  );
 
 		var list = element.all( by.repeater( 'item in list' ) );
+
+		expect( list.count(  ) ).toEqual( 1 );
+		expect( list.get( 0 ).element( by.model( 'item.name' ) ).getAttribute( 'value' ) ).toEqual( 'new list item' );
+	} );
+
+	it( 'should update an existing item', function(  )
+	{
+		browser.get( 'http://localhost:8080/app/list' );
+
+		element( by.model( 'newItem' ) ).sendKeys( 'new list item' );
+		element( by.css( 'div.enter-icon' ) ).click(  );
+
+		var list = element.all( by.repeater( 'item in list' ) );
+
 		expect( list.count(  ) ).toEqual( 1 );
 
-		console.log( list.get( 0 ).element( by.css( '.list-item-input' ) ) );
-		expect( list.get( 0 ).element( by.model( 'item.name' ) ).getAttribute( 'value' ) ).toEqual( 'new list item' );
+		var newListItemElement = list.get( 0 ).element( by.model( 'item.name' ) );
+
+		newListItemElement.clear(  );
+		newListItemElement.sendKeys( 'updated list item' );
+		newListItemElement.sendKeys( protractor.Key.ENTER );
+
+		expect( newListItemElement.getAttribute( 'value' ) ).toEqual( 'updated list item' );
 	} );
 } );
