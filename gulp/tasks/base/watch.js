1858e7527917df5ed6c72ab15f7c775a92e1ed0a
Ryan Warner <warner.ryan@gmail.com>
2016-09-23 10:15:14 -0700

    Update gulp tasks to use pug not jade

--- gulp/tasks/base/watch.js
+++ gulp/tasks/base/watch.js
@@ -10,12 +10,12 @@ var path        = require('../../paths.js');
 gulp.task('watch', function() {
 	gulp.watch(path.to.sass.source, ['sass']);
 
-	gulp.watch(path.to.jade.source, function() {
-		runSequence('jade', 'inject');
+	gulp.watch(path.to.pug.source, function() {
+		runSequence('pug', 'inject');
 	});
 
 	gulp.watch(path.to.scripts.source, function() {
-		runSequence('scripts', 'jade', 'inject');
+		runSequence('scripts', 'pug', 'inject');
 	});
 
 	gulp.watch(path.to.tests.source, ['eslint']);
--- gulp/tasks/default/pug.js
+++ gulp/tasks/default/pug.js
@@ -3,18 +3,18 @@
 var gulp    = require('gulp');
 var connect = require('gulp-connect');
 var cache   = require('gulp-cached');
-var jade    = require('gulp-jade');
+var pug    = require('gulp-pug');
 
 var path    = require('../../paths.js');
 var error   = require('../../error-handler.js');
 
 
 
-gulp.task('jade', function() {
-	return gulp.src(path.to.jade.source)
-		.pipe(cache('jade'))
-		.pipe(jade({ pretty: true }))
+gulp.task('pug', function() {
+	return gulp.src(path.to.pug.source)
+		.pipe(cache('pug'))
+		.pipe(pug({ pretty: true }))
 		.on('error', error.handler)
-		.pipe(gulp.dest(path.to.jade.destination))
+		.pipe(gulp.dest(path.to.pug.destination))
 		.pipe(connect.reload());
 });
